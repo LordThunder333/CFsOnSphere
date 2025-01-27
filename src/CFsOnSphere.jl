ce525bf5eb46681720dcc650c8b044d4c3252765
Mytraya Gattu <62602885+LordThunder333@users.noreply.github.com>
2025-01-27 15:20:30 -0500

    Update CFsOnSphere.jl

--- src/CFsOnSphere.jl
+++ src/CFsOnSphere.jl
@@ -14,7 +14,7 @@ LinearAlgebra.BLAS.set_num_threads(1)
 
 using JLD2
 
-export Ψproj, update_wavefunction!, rand_θ_ϕ_gen, proposal, arm_parameters, arm_scale_factor, legendre_polynomials!, save, load, logdet, lu, inv, update_density!
+export Ψproj, update_wavefunction!, gibbs_thermalization!, rand_θ_ϕ_gen, proposal, legendre_polynomials!, save, load, logdet, lu, inv, update_density!
 
 function update_density!(θmesh::Vector{Float64}, θcurrent::Vector{Float64}, accumulated_density::Vector{Float64})
     
@@ -23,7 +23,7 @@ function update_density!(θmesh::Vector{Float64}, θcurrent::Vector{Float64}, ac
     end
 
     return
-
+    
 end
 
 function update_density!(θmesh::Vector{Float64}, ϕmesh::Vector{Float64}, θcurrent::Vector{Float64}, ϕcurrent::Vector{Float64}, accumulated_density::Matrix{Float64})
@@ -35,6 +35,5 @@ function update_density!(θmesh::Vector{Float64}, ϕmesh::Vector{Float64}, θcur
     return
 
 end
-
 
 end
